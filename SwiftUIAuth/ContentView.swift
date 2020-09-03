a4ceadef430435f7965bb6fb76327ee790aea874
Karina <sarkisyan.karina@inbox.ru>
2020-09-03 22:40:30 +0300

    Initial Commit

---
+++ SwiftUIAuth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIAuth
+//
+//  Created by Karina Sarkisyan on 03.09.2020.
+//  Copyright © 2020 Karina Sarkisyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
